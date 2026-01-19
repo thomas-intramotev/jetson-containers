d5d7162b7a6a38b7a087a173fc2fcf360e433489
Thomas Kelly <thomas.kelly@intramotev.com>
2026-01-19 10:52:59 -0600

    Hardcode use of pypi registry in pytorch install script

--- packages/pytorch/install.sh
+++ packages/pytorch/install.sh
@@ -63,7 +63,7 @@ if [ "$PYTORCH_OFFICIAL_WHL" == "on" ]; then
 else
   # on x86_64, install from pytorch nightly server
   # on aarch64, install from the Jetson pypi server ($PIP_INSTALL_URL)
-  uv pip install torch==${TORCH_VERSION} || \
+  # uv pip install torch==${TORCH_VERSION} || \
   uv pip install --prerelease=allow "torch>=${PYTORCH_BUILD_VERSION}.dev,<=${PYTORCH_BUILD_VERSION}"
 fi
 
